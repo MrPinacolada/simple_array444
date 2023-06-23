02a8dcc8fe3754e54b42edfcdba66dd2e39c6d7c
BOB <vasiasko112@gmail.com>
2023-06-23 13:45:30 +0400

    add path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base:'/simple_array444',
   plugins: [
